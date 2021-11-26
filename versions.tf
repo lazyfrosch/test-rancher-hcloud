2b9e08a2c6f9e74f7e89a79b5ae20ccd206a4cfe
Markus Frosch <markus@lazyfrosch.de>
2021-11-26 12:50:10 +0100

    Update terraform providers

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     hetznerdns = {
       source  = "timohirt/hetznerdns"
-      version = "1.1.1"
+      version = ">= 1.1.1"
     }
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.31.0"
+      version = ">= 1.31.0"
     }
   }
   required_version = ">= 0.13"
